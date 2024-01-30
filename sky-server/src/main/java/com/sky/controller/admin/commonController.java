47d542a7544c86ee6312dde6059c05d8f1723b84
Ggxy159 <axyyu@outlook.com>
2024-01-30 21:55:18 +0800

    add new dish

--- sky-server/src/main/java/com/sky/controller/admin/commonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/commonController.java
@@ -1,22 +1,43 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/admin/common")
 @Api(tags = "common api")
+@Slf4j
 public class commonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
     @PostMapping("/upload")
     @ApiOperation("upload file")
-    public Result<String> upload(MultipartFile file){
+    public Result<String> upload(MultipartFile file) {
 
-        return
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String ObjectName = UUID.randomUUID() + extension;
+
+            String filePath = aliOssUtil.upload(file.getBytes(), ObjectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("fail to upload file {}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
